04582a8369e849a3c468c11ceb205e36dddf6984
viveksinghscheduleonce <viveksingh101994@gmail.com>
2021-02-28 19:44:38 +0530

    name changed to algoviz

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,7 +6,7 @@ import { setAlgo } from '../../redux/algorithm-selector-reducer/algorithm-select
 const HeaderComponent = ({ algorithms, setAlgoDispatch }) => {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Searching Algo-visualizer</Navbar.Brand>
+      <Navbar.Brand href="#home">Algo-viz</Navbar.Brand>
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
           <NavDropdown title="Select Algorithm" id="collasible-nav-dropdown">
